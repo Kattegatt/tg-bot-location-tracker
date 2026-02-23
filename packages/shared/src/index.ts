613e38c8f794d45b8f41353493fd858586b1b899
Denys Kostenko <kostenkodenys30@gmail.com>
2026-02-23 22:09:07 +0800

    60 symbols max comm

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,5 @@ export * from "./telegram";
 
 export const TTL_HOURS = 5;
 export const MERGE_DISTANCE_METERS = 200;
-export const COMMENT_MAX_CHARS = 200;
+export const COMMENT_MAX_CHARS = 60;
 export const COMMENT_MAX_WORDS = 25;
